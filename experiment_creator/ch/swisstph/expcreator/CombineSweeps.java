59e1bc0d839becc95448a33d921ffa0c01f54d7f
nicolas.maire@gmail.com <nicolas.maire@gmail.com@aa1ca794-fd11-11dd-ac65-f74f249cfaac>
2011-07-26 14:23:58 +0000

    ExperimentCreator: Start sweep group ids at 1 instead of zero
    git-svn-id: https://openmalaria.googlecode.com/svn@2072 aa1ca794-fd11-11dd-ac65-f74f249cfaac

--- experiment_creator/ch/swisstph/expcreator/CombineSweeps.java
+++ experiment_creator/ch/swisstph/expcreator/CombineSweeps.java
@@ -437,7 +437,9 @@ public class CombineSweeps {
             System.out.println("Inserted into experiments table");
 
             for (int i = 0; i < sweeps.size(); ++i) {
-                sweeps.get(i).updateDb(conn, i);
+                //Problem with zero as sweep group id in the visualization software
+                //therefore start sweep group ids at one instead of zero
+                sweeps.get(i).updateDb(conn, i+1);
             }
             System.out.println("Inserted into sweeps and arms tables");
 
